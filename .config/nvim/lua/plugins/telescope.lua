117c4ba14949a221dbf1357940afcd61fd86f44f
Luis De La Cruz <luisdanieldlcg@gmail.com>
2024-09-07 14:08:28 -0400

    Add telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,16 +3,17 @@ return {
 	dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local telescope = require("telescope")
-
         telescope.setup({
-           defaults = {
-                path_display = {"smart"}
-           },
-        });
+            defaults = require("telescope.themes").get_ivy(),
+        })
 
-        telescope.load_extension("fzf")
-
-        vim.keymap.set("n", "<leader>f", "<cmd> Telescope git_files<cr>", {})
-        vim.keymap.set("n", "<leader>pf", "<cmd> Telescope find_files<cr>", {})
+        vim.keymap.set("n", "<leader>f", function() 
+            local find = require("telescope.builtin")
+            local ok = pcall(find.git_files)
+            if not ok then 
+                find.find_files()
+            end
+        end)
+        vim.keymap.set("n", "<leader>pf", "<cmd> Telescope find_files <cr>", {})
     end
 }
