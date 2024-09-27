96506bfd993fc83d18a785b46b6a3b0e0701b465
luisdanieldlcg <luisdanieldlcg@gmail.com>
2024-09-27 09:14:24 -0400

    Send copy to sys clipboard

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -2,7 +2,7 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
-
+opt.clipboard:append("unnamedplus") -- copy to system clipboard
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
@@ -11,5 +11,4 @@ opt.smartindent = true
 
 opt.termguicolors = true
 opt.background = "dark"
-
 
