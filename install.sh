c0d7a7ed86c278f30e9a581428868f9b3d1ebd7f
luisdanieldlcg <luisdanieldlcg@gmail.com>
2025-02-08 20:07:42 -0400

    Simplify script

--- install.sh
+++ install.sh
@@ -3,28 +3,16 @@
 cd "$(dirname "$0")/.."
 DOTFILES=$HOME/dotfiles/.config
 
-
-install() {
-  
-  find -H "$DOTFILES" -name 'link.path' | while read linkfile
+find -H "$DOTFILES" -name 'link.path' | while read linkfile
+do
+  cat "$linkfile" | while read line
   do
-    cat "$linkfile" | while read line
-    do
-      local src dst dir
-      src=$(eval echo "$line" | cut -d '=' -f 1)
-      dst=$(eval echo "$line" | cut -d '=' -f 2)
-      dir=$(dirname "$dst")
-      echo "The directory: $dst will be linked (symlink) to $src"
-      mkdir -p "$dir"
-      link_file "$src" "$dst"
-    done  
-  done 
-}
-
-link_file() {
-  local src=$1 dst=$2
-  rm -rf $dst
-  ln -s $src $dst
- }
-
-install
+    src=$(eval echo "$line" | cut -d '=' -f 1)
+    dst=$(eval echo "$line" | cut -d '=' -f 2)
+    dir=$(dirname "$dst")gs 
+    echo "Linking $dst -> $src"
+    mkdir -p "$dir"
+    rm -rf $dst
+    ln -s $src $dst
+  done  
+done 
