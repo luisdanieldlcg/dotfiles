f61dada91c9f96a1536012ad9ce4753b253477d2
luisdanieldlcg <luisdanieldlcg@gmail.com>
2025-06-12 19:39:14 -0400

    nvim: create config open command

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
     "sainnhe/gruvbox-material",
+    lazy = false,
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme gruvbox-material]])
+	vim.g.gruvbox_material_background = 'hard'
+        vim.g.gruvbox_material_foreground = 'original'
+        -- vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_better_performance = 1
+        vim.cmd.colorscheme('gruvbox-material')
     end
 }
-
--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,7 +1,13 @@
 return {
-"ibhagwan/fzf-lua",
-    config = function()
-	local fzf = require("fzf-lua")
-	fzf.setup({})
+    "ibhagwan/fzf-lua",
+    opts = {},
+    init = function()
+        vim.api.nvim_create_user_command('ConfigOpen', function()
+            require('fzf-lua').files({ cwd = vim.fn.stdpath("config") })
+        end, {})
     end,
+    keys = {
+        { "<leader>f", function() require('fzf-lua').files() end },
+        { "<leader>/", function() require('fzf-lua').live_grep() end }
+    }
 }
